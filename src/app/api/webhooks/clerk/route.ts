98c65934ac6019d684aa0da2b7e68ed847807a39
krishkad <krishkadam88@gmail.com>
2024-03-19 14:05:53 +0530

    implement clerk webhooks

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -8,6 +8,7 @@ import { Webhook } from "svix";
 
 
 export async function POST(req: Request) {
+    console.log("POST")
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
